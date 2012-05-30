4adcdf3f585fe229d5338eabc6f5bfcb014e384a
Beth Brown <bbrown@cyrusinnovation.com>
2012-05-30 11:51:59 -0400

    basic histogram data

--- app/models/statistics_store.rb
+++ app/models/statistics_store.rb
@@ -30,6 +30,33 @@ class StatisticsStore < NSObject
       data
     end
   end
+  
+  def histogram
+    lates = day_of_week_counts(self.lates.collect(&:date))
+    outs = day_of_week_counts(self.sicks.collect(&:date))
+    
+    {
+      range: [0, range_from(lates, outs)],
+      lates: lates,
+      outs: outs
+    }
+  end
+  
+  def day_of_week_counts(dates)
+    result = {sunday:0, monday:0, tuesday:0, wednesday:0, thursday:0, friday:0,  saturday:0}
+    dates.each do |date|
+      symbol = [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday][date.wday]
+      result[symbol] += 1
+    end
+    
+    result
+  end
+  
+  def range_from(lates, outs)
+    possible_ranges = [1, 2, 3, 5, 8, 13, 20, 50, 100, 200, 366]
+    maximum = [lates.values.max, outs.values.max].max
+    possible_ranges.select {|u| u > maximum}.first
+  end
     
   def lates_this_month
     ailments_this_month("Late")
