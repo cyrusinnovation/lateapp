2ca77dcf9794eddf5deeb529720beae6742c8f46
Jason Reid <jason@jasonreid.com>
2012-06-12 16:09:05 -0400

    Added groups to emails stores

--- app/models/emails_store.rb
+++ app/models/emails_store.rb
@@ -7,20 +7,42 @@ class EmailsStore < BasicStore
     all('Email')
   end
   
+  def groups
+    all('Group')
+  end
+  
   def create_email
     email = create_managed_object_for_key('Email')
     email.email = ''
     email
-  end  
+  end
+  
+  def create_group
+    group = create_managed_object_for_key('Group')
+    group.name = ''
+    group
+  end
 
   def save_email(email)
     merge_new(email) unless email.email == ""
     persist
   end
   
+  def save_group(group)
+    merge_new(group) unless group.name == ''
+    persist
+  end
+  
   private
 
   def initialize
-    super(['Email'], 'Emails.sqlite')
+    super(['Email', 'Group'], 'Emails.sqlite')
+    
+    if groups == []
+      puts "No groups found. Creating default group 'Work'"
+      work = create_group
+      work.name = 'Work'
+      save_group(work)
+    end
   end
 end
